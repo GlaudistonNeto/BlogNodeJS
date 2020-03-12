b0bd6b9bd37dab0158e1fd7ea7211aedc6076a83
GNeto <glaudistonson@gmail.com>
2020-03-12 01:58:55 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database/database');
+
+//View engine
+app.set('view engine', 'ejs');
+
+//Static
+app.use(express.static('public'));
+
+//Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Database
+connection
+  .authenticate()
+  .then(() => {
+    console.log('Connected to database');
+  }).catch((error) => {
+    console.log(error);
+  });
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('Server running!');
+});
