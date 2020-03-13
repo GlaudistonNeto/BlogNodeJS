24c8d769721e4745eae4a6f16f1c5c48e01f4783
GNeto <glaudistonson@gmail.com>
2020-03-13 01:42:26 -0300

    blog functionalities added

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('gnetopress', 'root', 'dev320', {
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  timezone: '-03:00'
 });
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/database');
 
+const categoriesController = require('./categories/CategoriesController');
+const articlesController = require('./articles/ArticlesController');
+
+const Article = require('./articles/Article');
+const Category = require('./categories/Category');
+
 //View engine
 app.set('view engine', 'ejs');
 
@@ -22,8 +28,35 @@ connection
     console.log(error);
   });
 
+app.use('/', categoriesController);
+
+app.use('/', articlesController);
+
 app.get('/', (req, res) => {
-  res.render('index');
+  Article.findAll({
+    order:[
+      ['id', 'DESC']
+    ]
+  }).then(articles => {
+    res.render('index', {articles: articles});
+  });
+});
+
+app.get('/:slug', (req, res) => {
+  var slug = req.params.slug;
+  Article.findOne({
+    where: {
+      slug: slug
+    }
+  }).then(article => {
+    if(article != undefined){
+      res.render('article', {article: article});
+    }else{
+      res.redirect('/');
+    }
+  }).catch(err => {
+    res.redirect('/');
+  });
 });
 
 app.listen(3000, () => {
