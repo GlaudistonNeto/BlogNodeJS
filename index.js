053bc8e8f33f219f571aeccfdd6cb19ffb45dc0c
GNeto <glaudistonson@gmail.com>
2020-03-14 18:50:56 -0300

    bugs fixed

--- index.js
+++ index.js
@@ -9,80 +9,84 @@ const articlesController = require('./articles/ArticlesController');
 const Article = require('./articles/Article');
 const Category = require('./categories/Category');
 
-//View engine
-app.set('view engine', 'ejs');
+// View engine
+app.set('view engine','ejs');
 
-//Static
+// Static
 app.use(express.static('public'));
 
 //Body parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-//Database
+// Database
+
 connection
-  .authenticate()
-  .then(() => {
-    console.log('Connected to database');
-  }).catch((error) => {
-    console.log(error);
-  });
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com sucesso!");
+    }).catch((error) => {
+        console.log(error);
+    })
 
-app.use('/', categoriesController);
 
-app.use('/', articlesController);
+app.use("/",categoriesController);    
+app.use("/",articlesController);
 
-app.get('/', (req, res) => {
-  Article.findAll({
-    order:[
-      ['id', 'DESC']
-    ]
-  }).then(articles => {
-    Category.findAll().then(categories => {
-      res.render('index', {articles: articles, categories: categories});
+
+app.get("/", (req, res) => {
+    Article.findAll({
+        order:[
+            ['id','DESC']
+        ],
+        limit: 4
+    }).then(articles => {
+        Category.findAll().then(categories => {
+            res.render("index", {articles: articles, categories: categories});
+        });
     });
-  });
-});
+})
 
-app.get('/:slug', (req, res) => {
-  var slug = req.params.slug;
-  Article.findOne({
-    where: {
-      slug: slug
-    }
-  }).then(article => {
-    if(article != undefined){
-      Category.findAll.then(categories => {
-        res.render('article', {article: article, categories: categories});
-      });
-    }else{
-      res.redirect('/');
-    }
-  }).catch(err => {
-    res.redirect('/');
-  });
-});
+app.get("/:slug",(req, res) => {
+    var slug = req.params.slug;
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories => {
+                res.render("article", {article: article, categories: categories});
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch( err => {
+        res.redirect("/");
+    });
+})
 
-app.get('/category/:slug', (req, res) => {
-  var slug = req.params.slug;
-  Category.findOne({
-    where: {
-      slug: slug
-    },
-    include: [{model: Article}]
-  }).then(category => {
-    if(category != undefined){
-      Category.findAll().then(categories => {
-        res.render("index", {articles: category.articles, categories: categories})
-      });
-    }else{
-      res.redirect('/');
-    }
-  }).catch(err => {
-    res.redirect('/');
-  });
-});
+app.get("/category/:slug",(req, res) => {
+    var slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then( category => {
+        if(category != undefined){
+            Category.findAll().then(categories => {
+                res.render("index",{articles: category.articles,categories: categories});
+            });
+        }else{
+            res.redirect("/");
+        }
+    }).catch( err => {
+        res.redirect("/");
+    })
+})
+
 
 app.listen(3000, () => {
-  console.log('Server running!');
-});
+    console.log("O servidor está rodando!")
+})
